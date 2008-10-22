2c6ab5755108f233a5bc36ecea253e55f71c7639
Stephen Tudor <smt@sunblu.sh>
2008-10-22 13:42:59 -0400

    Adding fractionize helper method

--- lib/sunblush/fractions_helper.rb
+++ lib/sunblush/fractions_helper.rb
@@ -1,12 +1,28 @@
 module Sunblush
   module FractionsHelper
-    def nice_fraction(float, options={})
-      f = float.to_fraction
+    def nice_fraction(num, options={})
+      f = num.to_fraction
       opts = {:html=>false, :top=>'sup', :bottom=>'sub'}.merge!(options)
       if opts[:html]
         "<span class=\"fraction\"><#{opts[:top]}>#{f.first}</#{opts[:top]}>&frasl;<#{opts[:bottom]}>#{f.last}</#{opts[:bottom]}></span>"
       else
         "#{f.first}/#{f.last}"
+      end
+    end
+  
+    def fractionize(num, options={})
+      opts = { :html => true, :dash => false }.merge!(options)
+      if num.to_f == 0.0
+        0
+      elsif num.to_f - num.to_i == 0.0
+        num.to_i
+      elsif num.to_f < 1.0
+        nice_fraction(num.to_f, opts)
+      else
+        str = "#{num.to_i}"
+        str += opts[:dash] == true ? "-" : " "
+        str += "#{nice_fraction((num.to_f - num.to_i), opts)}"
+        str
       end
     end
   end
